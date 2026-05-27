0ba126c1687b42025408ffaa653c2e59541a1e45
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2026-05-27 08:15:51 +0200

    feat: setup supabase
    Setup Supabase clients:
    - server
    - browsesr

---
+++ utils/supabase/client.ts
@@ -0,0 +1,14 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+	throw new Error("NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY variables not set")
+}
+
+export const createClient = () =>
+  createBrowserClient(
+    supabaseUrl!,
+    supabaseKey!,
+  );
